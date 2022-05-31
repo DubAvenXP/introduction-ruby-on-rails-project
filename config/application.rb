9fc85905b6a364d656329bd27a1cb46c2b840348
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-05-31 08:18:51 -0600

    [Working] testing with spec

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,16 @@ module LomaxProject
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework(
+        :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false
+        routing_specs: false
+      )
+    end
   end
 end
